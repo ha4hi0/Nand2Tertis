68a269feaf972687c6e3408ab3d70be7496b86a6
ha4hi0 <37325679+ha4hi0@users.noreply.github.com>
2023-01-30 18:22:43 +0900

    Nand 2 Program Counter

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,29 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+// initialize
+@i
+M=0
+@R2
+M=0
+
+//  loop
+(LOOP)
+// if(i>=R1) goto end
+@i
+D=M
+@R1
+D=M-D
+@END
+D;JGE
+
+@R0
+D=M
+@R2
+D=D+M
+
+@i
+M=M+1
+
+(END)
